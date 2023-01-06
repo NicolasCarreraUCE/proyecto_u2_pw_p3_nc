67192ecf8e8393c7e5320ea92bac1d09a077fb57
Juan Estrella <nacarrera@uce.edu.ec>
2023-01-05 19:44:20 -0500

    taller 16

---
+++ src/components/AppContador.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Componente principal</h1>
+  <Contador titulo="Estudiante" v-bind:inicio="101"/>
+  <Contador titulo="Profesor" v-bind:inicio="200"/>
+  <!--<Contador titulo="Test" :inicio="11*20"/>-->
+</template>
+
+<script>
+  import Contador from './components/Contador.vue'
+  export default {
+    name: 'App',
+    components: {
+      Contador
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
